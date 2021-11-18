c6acc89c9a590be1a0d516afaa63281e8e20b704
Alex Vandiver <alexmv@zulip.com>
2021-11-18 00:44:51 +0000

    camo: Replace with go-camo implementation.
    The upstream of the `camo` repository[1] has been unmaintained for
    several years, and is now archived by the owner.  Additionally, it has
    a number of limitations:
     - It is installed as a sysinit service, which does not run under
       Docker
     - It does not prevent access to internal IPs, like 127.0.0.1
     - It does not respect standard `HTTP_proxy` environment variables,
       making it unable to use Smokescreen to prevent the prior flaw
     - It occasionally just crashes, and thus must have a cron job to
       restart it.
    
    Swap camo out for the drop-in replacement go-camo[2], which has the
    same external API, requiring not changes to Django code, but is more
    maintained.  Additionally, it resolves all of the above complaints.
    
    go-camo is not configured to use Smokescreen as a proxy, because its
    own private-IP filtering prevents using a proxy which lies within that
    IP space.  It is also unclear if the addition of Smokescreen would
    provide any additional protection over the existing IP address
    restrictions in go-camo.
    
    Fixes #18351 by moving to supervisor.
    Fixes zulip/docker-zulip#298 also by moving to supervisor.
    
    [1] https://github.com/atmos/camo
    [2] https://github.com/cactus/go-camo

--- puppet/zulip/manifests/camo.pp
+++ puppet/zulip/manifests/camo.pp
@@ -1,22 +1,43 @@
 class zulip::camo {
-  $camo_packages = [# Needed for camo
-                    'nodejs',
-                    'camo',
-                    ]
-  package { $camo_packages: ensure => 'installed' }
+  package { 'camo':
+    ensure => 'purged',
+  }
+
+  $version = '2.3.0'
+
+  $dir = "/srv/zulip-go-camo-${version}/"
+  $bin = "${dir}bin/go-camo"
+
+  zulip::sha256_tarball_to { 'go-camo':
+    url     => "https://github.com/cactus/go-camo/releases/download/v${version}/go-camo-${version}.go1171.linux-amd64.tar.gz",
+    sha256  => '965506e6edb9d974c810519d71e847afb7ca69d1d01ae7d8be6d7a91de669c0c',
+    install => {
+      "go-camo-${version}/" => $dir,
+    },
+  }
+  file { $dir:
+    ensure  => directory,
+    require => Zulip::Sha256_tarball_to['go-camo'],
+  }
+  tidy { '/srv/zulip-go-camo-*':
+    path    => '/srv/',
+    recurse => 1,
+    rmdirs  => true,
+    matches => 'zulip-go-camo-*',
+    require => File[$dir],
+  }
 
   $camo_key = zulipsecret('secrets', 'camo_key', '')
-
-  file { '/etc/default/camo':
+  file { "${zulip::common::supervisor_conf_dir}/go-camo.conf":
     ensure  => file,
-    require => Package[camo],
+    require => [
+      Package[supervisor],
+      File[$dir],
+    ],
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    content => template('zulip/camo_defaults.template.erb'),
-    notify  => Service[camo],
-  }
-  service { 'camo':
-    ensure => running,
+    content => template('zulip/supervisor/go-camo.conf.erb'),
+    notify  => Service[supervisor],
   }
 }
